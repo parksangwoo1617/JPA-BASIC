d1a09db7bb43c19716560b28c25cd287a8902167
parksangwoo <b01056721617@gmail.com>
2021-07-28 15:57:41 +0900

    📑 :: JpaMain

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,2 +1,17 @@
-package hellojpa;public class JpaMain {
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager entityManager = emf.createEntityManager();
+        //code
+        entityManager.close();
+
+        emf.close();
+    }
 }
